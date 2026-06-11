4362832cc0c7ce36d1495499e725fa48176b5cf7
jatin-05dev <jatindev214@gmail.com>
2026-06-11 09:25:09 +0530

    code

---
+++ recursion/first.py
@@ -0,0 +1,62 @@
+# for i in range(1,11):
+#     print(i,end=" ")
+
+# def rec(s,n):
+#     if s==n:
+#         return
+#     print(s,end=" ")
+#     rec(s+1,n)
+
+# rec(1,11)
+
+# head recursion
+
+
+# def rec(s,n):
+#     if s==n:
+#         return
+#     rec(s+1,n)
+#     print(s,end=" ")
+
+
+# rec(1,11)
+
+# tail recursion
+
+# for loop factorial
+
+# def fac(n):
+#     fact = 1
+
+#     for i in range(1, n + 1):
+#         fact = fact * i
+
+#     print(fact)
+
+# fac(5)
+# from recursion
+
+# def fac(n):
+#     if n==1:
+#         return n
+#     return n*fac(n-1)
+
+# fac(5)
+    
+# tablr program
+# power
+def table(n):
+    if n==1:
+        return n
+    return n*table(n)
+
+table(5)
+
+
+    
+
+
+
+
+
+
